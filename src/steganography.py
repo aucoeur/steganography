771aa9bce9b61acd177296652477deeb8ddd5df0
aucoeur <aucoeur.ngo@students.makeschool.com>
2021-04-26 11:45:58 -0700

    finished decode :D

--- src/steganography.py
+++ src/steganography.py
@@ -13,21 +13,32 @@ from PIL import Image
 
 def decode_image(path_to_png):
     """
-    TODO: Add docstring and complete implementation.
+    Checks red channel for LSB(least significant bit) to output hidden text from encoded png
     """
     # Open the image using PIL:
     encoded_image = Image.open(path_to_png)
 
     # Separate the red channel from the rest of the image:
     red_channel = encoded_image.split()[0]
+    red_pixels = red_channel.load()
 
     # Create a new PIL image with the same size as the encoded image:
     decoded_image = Image.new("RGB", encoded_image.size)
     pixels = decoded_image.load()
     x_size, y_size = encoded_image.size
 
-    # TODO: Using the variables declared above, replace `print(red_channel)` with a complete implementation:
-    print(red_channel)  # Start coding here!
+
+    # Using the variables declared above, replace `print(red_channel)` with a complete implementation:
+    # print(red_channel)
+
+    for x in range(0, x_size):
+        for y in range(0, y_size):
+            # print(f"{red_pixels[x, y]:08b}", red_pixels[x, y])
+
+            if red_pixels[x, y] & 1 == 1:
+                pixels[x,y] = (255, 255, 255)
+            elif red_pixels[x, y] & 1 == 0:
+                pixels[x,y] = (0, 0, 0)
 
     # DO NOT MODIFY. Save the decoded image to disk:
     decoded_image.save("decoded_image.png")
@@ -35,14 +46,14 @@ def decode_image(path_to_png):
 
 def encode_image(path_to_png):
     """
-    TODO: Add docstring and complete implementation.
+    Encodes an image with a secret message on the red channel by tweaking its LSB
     """
     pass
 
 
 def write_text(text_to_write):
     """
-    TODO: Add docstring and complete implementation.
+    Creates temp image of given text to use when encoding image with secret message
     """
     pass
 
