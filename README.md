3cd0a6eab95d2c04c3a2e7f78c9eba46e12e64eb
aucoeur <aucoeur.ngo@students.makeschool.com>
2021-04-30 18:16:38 -0700

    bit shenanigans

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Steganography with Python and PIL
 
 Encoding and decoding on the red channel with the LSB
+
+## Notes on Bitwise Operations
+Operator | Name |	Description	| Example
+-|-|-|-|
+`&` | `AND` | copies bit if exists in **BOTH** operands |	`a & b`
+`\|` | `OR` | copies bit if exists in **EITHER** operand.	| `a \| b`
+`^` |  `XOR` |	copies bit if in **ONE BUT NOT BOTH**. |	`a ^ b`
+`~` | Unary Complement | Has the effect of **'FLIPPING'** bits. |	`~ a`
+`<<` |  Binary Left Shift |	left value is moved left by num bits of right value. |	`a << 2` 
+`>>` | Binary Right Shift	|  left value is moved right by num bits of right value. |	`a >> 2`
--- src/steganography.py
+++ src/steganography.py
@@ -9,7 +9,7 @@ Deliverables:
     3. Your own image encoded with hidden secret text!
 """
 # TODO: Run `pip3 install Pillow` before running the code.
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageChops, ImageDraw, ImageFont
 
 def decode_image(path_to_png):
     """
@@ -25,8 +25,8 @@ def decode_image(path_to_png):
     # Create a new PIL image with the same size as the encoded image:
     decoded_image = Image.new("RGB", encoded_image.size)
     pixels = decoded_image.load()
-    x_size, y_size = encoded_image.size
 
+    x_size, y_size = encoded_image.size
 
     # Using the variables declared above, replace `print(red_channel)` with a complete implementation:
     # print(red_channel)
@@ -36,73 +36,69 @@ def decode_image(path_to_png):
             # print(f"{red_pixels[x, y]:08b}", red_pixels[x, y])
 
             # bitwise AND operator (&) returns a 1 in each bit position for which the corresponding bits of both operands are 1
-            # nb. LSB for odd numbers always 1, even numbers always 0
+            # note: LSB for odd numbers always 1, even numbers always 0 (ie. n % 2)
             if red_pixels[x, y] & 1 == 0:
                 pixels[x,y] = (255, 255, 255)
             else:
-                # if red_pixels[x, y] & 1 == 0:
+                # if red_pixels[x, y] & 1 == 1:
                 pixels[x,y] = (0, 0, 0)
 
     # DO NOT MODIFY. Save the decoded image to disk:
-    # decoded_image.save("decoded_image.png")
-    decoded_filename = path_to_png.split('.')
-    decoded_image.save(f'{decoded_filename[0]}-decoded.png')
+    decoded_image.save("decoded_image.png")
 
+    # decoded_filename = path_to_png.split('.')
+    # decoded_image.save(f'{decoded_filename[0]}-decoded.png')
 
 def encode_image(path_to_png, msg):
     """
     Encodes an image with a secret message on the red channel by tweaking its LSB
     """
     base_image = Image.open(path_to_png)
-    pixels = base_image.load()
-
-    redc = base_image.split()[0]
-    greenc = base_image.split()[1]
-    bluec = base_image.split()[2]
-
-    red = redc.load()
-    green = greenc.load()
-    blue = bluec.load()
-
     x_width, y_height = base_image.size
 
-    hiddenText = write_text(msg, base_image.size)
+    pixels = base_image.load()
 
-    secret_red = hiddenText.split()[0]
-    secret_pixel = secret_red.load()
+    hidden_text = write_text(msg, base_image.size)
+    secret_pixels = hidden_text.load()
 
     for x in range(x_width):
         for y in range(y_height):
-            red_flag = bin(red[x,y])[-1]
-            text_flag = bin(secret_pixel[x,y])[-1]
 
-            if int(text_flag[-1]) & 1 == 1:
-                red[x,y] = int(bin(red[x,y])[:-1]+"1", 2)
-            elif int(text_flag[-1]) & 1 == 0:
-                red[x,y] = int(bin(red[x,y])[:-1]+"0", 2)
+            red, green, blue, alpha = pixels[x,y]
+            text_flag =  secret_pixels[x,y]
 
-            pixels[x,y] = (red[x, y], green[x,y], blue[x,y])
+            if text_flag == 1:
+                # print(red, text_flag, red | text_flag)
+                red = red | text_flag
+            else:
+                # note: LSB for odd numbers always 1, even numbers always 0 (ie. n % 2)
+                if red % 2 == 1:
+                    red = red ^ 1
+
+            pixels[x,y] = (red, green, blue, alpha)
 
     new_filename = path_to_png.split('.')
     base_image.save(f'{new_filename[0]}-encoded.png')
+
 
 def write_text(text_to_write, size):
     """
     Creates temp image of given text to use when encoding image with secret message
     """
-    image = Image.new("RGB", size, (0, 0, 0))
+    image = Image.new('RGBA', size, (255, 255, 255, 0))
 
     font = ImageFont.truetype('/Library/Fonts/AvenirLTStd-Black.otf', 24)
 
     draw = ImageDraw.Draw(im=image)
 
-    draw.text((25, 200), text_to_write, font=font, fill=(255, 255, 255, 255))
+    draw.text((25, 200), text_to_write, font=font, fill=(255, 255, 255, 1))
 
-    return image
+    return image.getchannel("A")
 
 
 if __name__ == "__main__":
     # decode_image('src/bear_dog.png')
-    msg = 'candy camouflage!'
-    encode_image('src/over_the_garden_wall.png', msg)
-    decode_image('src/over_the_garden_wall-encoded.png')
+    # msg = 'candy camouflage!'
+    msg = 'yessir young man!'
+    encode_image('src/otgw.png', msg)
+    decode_image('src/otgw-encoded.png')
